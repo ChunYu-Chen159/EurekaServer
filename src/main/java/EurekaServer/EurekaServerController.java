fdbab0183799aeb3c15e70840b1ac8cbe0c66b6f
Chen <i11007020@gmail.com>
2019-03-04 06:55:12 +0800

    commit

--- src/main/java/EurekaServer/EurekaServerController.java
+++ src/main/java/EurekaServer/EurekaServerController.java
@@ -2,15 +2,11 @@ package EurekaServer;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class EurekaServerController {
-	
 	@RequestMapping(value="/", method = RequestMethod.GET)
     public String index() 
     {
 		return "success";
     }
-	
 }
