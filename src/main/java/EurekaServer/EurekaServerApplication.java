90a7ed0ba5ceba188f27e52562585249b7cd2fb4
Chen <i11007020@gmail.com>
2019-08-07 15:08:10 +0800

    commit

--- src/main/java/EurekaServer/EurekaServerApplication.java
+++ src/main/java/EurekaServer/EurekaServerApplication.java
@@ -3,6 +3,7 @@ package EurekaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import com.soselab.vmamveurekaclient.annotation.EnableVmamvRegister;
 
 @SpringBootApplication
 @EnableEurekaServer
