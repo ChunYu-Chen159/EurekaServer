6819c680830ab1029376250e892353c8cf83f668
Chen <i11007020@gmail.com>
2019-08-07 15:05:27 +0800

    commit

--- src/main/java/EurekaServer/EurekaServerApplication.java
+++ src/main/java/EurekaServer/EurekaServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableVmamvRegister
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
